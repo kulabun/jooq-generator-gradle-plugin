c3e81cff28232d8495e486c06dcabd39ff420e10
Konstantin Labun <konstantin.labun@gmail.com>
2018-02-18 09:32:42 +0100

    prepublish preparations

--- src/main/groovy/org/labun/jooq/generator/gradle/JooqCodeGenerationExtension.groovy
+++ src/main/groovy/org/labun/jooq/generator/gradle/JooqCodeGenerationExtension.groovy
@@ -1,17 +1,14 @@
 package org.labun.jooq.codegenng.gradle
 
 import org.labun.jooq.codegen.config.NameConfig
+
 /**
  * @author Konstantin Labun
  */
 class JooqCodeGenerationExtension {
     Closure database
     List<Closure> generators
-    //
-//    JooqExtension(Configuration configuration) {
-//        this.configuration = configuration
-//    }
-//
+
     def methodMissing(String configName, args) {
         if ('nameConfig'.equals(configName)) return new NameConfig()
     }
