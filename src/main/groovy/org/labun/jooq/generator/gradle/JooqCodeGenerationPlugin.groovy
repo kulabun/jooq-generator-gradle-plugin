c59cca918d855d1fb5660b8691b8fc676938ec6e
Konstantin Labun <konstantin.labun@gmail.com>
2018-05-03 23:32:30 +0300

    update to 2.0-SNAPSHOT codegenerator

--- src/main/groovy/org/labun/jooq/generator/gradle/JooqCodeGenerationPlugin.groovy
+++ src/main/groovy/org/labun/jooq/generator/gradle/JooqCodeGenerationPlugin.groovy
@@ -3,11 +3,11 @@ package org.labun.jooq.generator.gradle
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.labun.jooq.generator.DefaultGenerator
+import org.labun.jooq.generator.Defaults
 import org.labun.jooq.generator.GenerationTool
-import org.labun.jooq.generator.config.CodeGenerationConfig
 import org.labun.jooq.generator.config.Configuration
 import org.labun.jooq.generator.config.DatabaseConfig
-import org.labun.jooq.generator.config.Defaults
+import org.labun.jooq.generator.config.SubGeneratorConfig
 
 /**
  * @author Konstantin Labun
@@ -17,7 +17,7 @@ class JooqCodeGenerationPlugin implements Plugin<Project> {
     void apply(Project project) {
         def configuration = new Configuration()
         configuration.database = new DatabaseConfig()
-        configuration.database.schemas(["PUBLIC"])
+        configuration.database.schemas = ["PUBLIC"]
 
         JooqCodeGenerationExtension extension = project.extensions.create("jooqCodeGeneration", JooqCodeGenerationExtension.class)
 
@@ -25,26 +25,32 @@ class JooqCodeGenerationPlugin implements Plugin<Project> {
             doLast {
                 configuration.database.with(extension.database)
 
-                List<CodeGenerationConfig> defaultConfigs = Defaults.CodeGenerationConfigs.all()
+                List<SubGeneratorConfig> defaultConfigs = new Defaults.CodeGenerationConfigs().all()
 
-                def sourceRootConfigurer = { it.generatedSourcesRoot = "${project.getBuildDir()}/generated/src/main/java/" }
+                def sourceRootConfigurer = {
+                    it.generatedSourcesRoot = "${project.getBuildDir()}/generated/src/main/java/"
+                }
 
+                def subGenerators = new ArrayList<>(configuration.subGenerators)
                 extension.generators.each({ cfg ->
-                    def generator = new CodeGenerationConfig()
+                    def generator = new SubGeneratorConfig()
                     generator.with(sourceRootConfigurer)
                     generator.with(cfg)
 
-                    def defaultGenerator = defaultConfigs.find({ it.generatorName == generator.generatorName })
+                    def defaultGenerator = defaultConfigs.find({
+                        it.generatorName == generator.generatorName
+                    })
                     if (defaultGenerator != null) {
                         generator = defaultGenerator
                         generator.with(sourceRootConfigurer)
                         generator.with(cfg)
                     }
 
-                    configuration.codeGeneration.add(generator)
+                     subGenerators.add(generator)
                 })
 
-                GenerationTool.generate(new DefaultGenerator(configuration))
+                configuration.subGenerators = subGenerators
+                new GenerationTool().generate(new DefaultGenerator(configuration))
             }
         }
     }
